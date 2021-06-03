fb944501a7ce8b7dde5279d918975b14723e0014
Ayesha94 <gupta.ayesh@gmail.com>
2021-06-03 23:48:49 +0530

    stellar stage 1

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import DailyPicScreen from './screens/DailyPic'
+import StarMapScreen from './screens/StarMap'
+import SpacecraftScreen from './screens/SpaceCraft'
+import 'react-native-gesture-handler'
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
+import HomeScreen from './screens/HomeScreen'
 
+const Stack=createStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown:false}}>
+        <Stack.Screen name="Home" component={HomeScreen}/>
+        <Stack.Screen name='SpaceCrafts' component={SpacecraftScreen}/>
+        <Stack.Screen name="DailyPic" component={DailyPicScreen}/>
+        <Stack.Screen name="StarMap" component={StarMapScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
